c482ce97da07dd864d1963bed7a8e315eefb54ff
Andrey Koleshko <ka8725@gmail.com>
2012-03-30 01:19:04 +0300

    remove spaces

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,2 +1,3 @@
+raise Rails.env.production?.inspect
 MongoMapper.connection = Rails.env.production? ? Mongo::Connection.from_uri(ENV['MONGOLAB_URI']) : Mongo::Connection.new('localhost', 27017)
 MongoMapper.database = Rails.env.production? ? "heroku_app3547355" : "gem_stats"
