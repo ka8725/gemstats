120ee861217e717fba15b9d778c1dce8499ac8f4
Andrey Koleshko <ka8725@gmail.com>
2012-03-30 03:26:33 +0300

    fix connection

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,1 +1,3 @@
-GemStats::Connection.connect
+config = {'development' => {'uri' => 'mongodb://localhost:27017/gem_stats'},
+          'production'  => {'uri' => ENV['MONGOLAB_URI']}}
+GemStats::Connection.connect(:config => config, :env => Rails.env)
