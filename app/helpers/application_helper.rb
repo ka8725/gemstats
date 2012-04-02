9a1b218915f6d0026c41597a5ae0099d480974ab
Andrey Koleshko <ka8725@gmail.com>
2012-04-02 14:28:12 +0300

    some changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
       <td>
         <div class="label label-inverse">#{name}</div>
         <ul class='gem-list'>
-          #{list.map { |gem_name| "<li>#{gem_name}</li>".html_safe }.join.html_safe}
+          #{list.map { |gem_name| "<li title='#{gem_name}'>#{gem_name}</li>".html_safe }.join.html_safe}
         </ul>
         <dl class='dl-horizontal label'>
           <dt>Total:</dt>
