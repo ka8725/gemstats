2ce91232c6259ceb3cb2d7b55b7c12115339ecdf
Andrey Koleshko <ka8725@gmail.com>
2012-04-02 13:43:03 +0300

    design changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,19 @@ module ApplicationHelper
     @clients ||= {}
     @clients[env] ||= GemStats::Gem.where("#{env}_dependencies" => @gem_info.name).map(&:name)
   end
+
+  def gems_info(list, name)
+    %Q{
+      <td>
+        <div class="label label-inverse">#{name}</div>
+        <ul class='gem-list'>
+          #{list.map { |gem_name| "<li>#{gem_name}</li>".html_safe }.join.html_safe}
+        </ul>
+        <dl class='dl-horizontal label'>
+          <dt>Total:</dt>
+            <dd>#{list.count}</dd>
+        </dl>
+      </td>
+    }.html_safe
+  end
 end
