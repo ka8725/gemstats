9e47e461116c99b9ee3e04137dbf1de8f677c787
Andrey Koleshko <ka8725@gmail.com>
2012-04-04 01:37:29 +0300

    README & spec

---
+++ README.md
@@ -0,0 +1,5 @@
+# Gem Stats
+This is a website which provides full statistics for all gems from [rubygems](http://rubygems.org). The most important feature is that you are able to get dependent gems. Visit [homepage](http://metagem.info) to check it out.
+
+## Contributing
+You are welcome
--- spec/factories/gem_infos.rb
+++ spec/factories/gem_infos.rb
@@ -2,8 +2,23 @@
 
 FactoryGirl.define do
   factory :gem, :class => GemStats::Gem do
-    name        'my_gem_name'
-    version     '0.0.1'
-    downloads   111
+    sequence(:name)   {|n| "my_gem_name#{n}"}
+    version           '0.0.1'
+    version_downloads {rand(500)}
+    downloads         {rand(1000)}
+    info              "This is info"
+    authors           "Green Budget"
+    dependencies      "{'development' : [{'name' : 'my_gem_0', 'requirements' : '0.0.1' },
+                                         {'name' : 'my_gem_1', 'requirements' : '0.0.1' }],
+                        'runtime'     : [{'name' : 'my_gem_3', 'requirements' : '0.0.1' },
+                                         {'name' : 'my_gem_4', 'requirements' : '0.0.1' }]}"
+    project_uri             "http://project-uri.com"
+    gem_uri                 "http://project-uri.com/gem.gem"
+    homepage_uri            "http://homepage-uri.com"
+    wiki_uri                "http://wiki-uri.com"
+    documentation_uri       "http://documentation-uri.com"
+    mailing_list_uri        "http://mailing-list-uri.com"
+    bug_tracker_uri         "http://bug-tracker-uri.com"
+    source_code_uri         "http://source-code-uri.com"
   end
 end
