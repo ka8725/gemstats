0e7047cdcecce65c022d791c605ab456ca1a8860
Andrey Koleshko <ka8725@gmail.com>
2012-04-03 18:28:29 +0300

    fix env

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,6 +1,6 @@
 config = {'development' => {'uri' => 'mongodb://localhost:27017/gem_stats'},
           'test'        => {'uri' => 'mongodb://localhost:27017/gem_stats_test'},
           'production'  => {'uri' => ENV['MONGOLAB_URI']}}
-env = Rails.groups.include?('assets') ? 'development' : 'production'
-GemStats::Connection.connect(:config => config, :env => env)
+env = Rails.groups.include?('assets') ? 'development' : Rails.env
+GemStats::Connection.connect(:config => config, :env => env).inspect
 GemStats::Gem.ensure_index :name
